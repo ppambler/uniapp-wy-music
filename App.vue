0da2c5c0049e85ef79476f13848e8f0cc5313a7a
ppambler <allinlbj@gmail.com>
2022-01-24 17:19:25 +0800

    update

--- App.vue
+++ App.vue
@@ -1,5 +1,4 @@
 <script>
-<<<<<<< HEAD
 export default {
   onLaunch: function () {
     console.log("App Launch");
@@ -17,21 +16,4 @@ export default {
 /*每个页面公共css */
 @import "@/common/css/common.scss";
 @import "@/common/css/iconfont.css";
-=======
-	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
-	}
-</script>
-
-<style>
-	/*每个页面公共css */
->>>>>>> 169aa268565a132fbe3b647d2b326a7ae33c42bc
 </style>
--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import App from "./App";
 
 // #ifndef VUE3
@@ -25,26 +24,3 @@ export function createApp() {
   };
 }
 // #endif
-=======
-import App from './App'
-
-// #ifndef VUE3
-import Vue from 'vue'
-Vue.config.productionTip = false
-App.mpType = 'app'
-const app = new Vue({
-    ...App
-})
-app.$mount()
-// #endif
-
-// #ifdef VUE3
-import { createSSRApp } from 'vue'
-export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
-}
-// #endif
->>>>>>> 169aa268565a132fbe3b647d2b326a7ae33c42bc
